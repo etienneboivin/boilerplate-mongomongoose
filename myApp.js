0891ef9a911e261f8343ec1b57b71d48c0f6b399
atms <etiennedtboivin@gmail.com>
2022-05-23 11:39:25 -0700

    Fixed more syntax error

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const personSchema = new Schema({
-  name: {type: String, required: true}
+  name: {type: String, required: true},
   age: Number,
   favoriteFoods: [String]
 });
